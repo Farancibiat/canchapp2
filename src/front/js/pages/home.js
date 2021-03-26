b2ed67f08c48f0464173c722500101a6c5ce5f81
codehsalas <hectorsalas7@gmail.com>
2021-03-26 00:32:34 +0000

    Update Navbar & Home

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,10 +1,12 @@
 import React from "react";
+import imageHome from "/workspace/canchapp2/src/front/img/home.png";
+import "../styles/home.css";
 // import { Context } from "../store/appContext";
 
 export const Home = () => {
 	return (
-		<div>
-			<h1>Home</h1>
-		</div>
+		<>
+			<img className="imgHome" src={imageHome} />
+		</>
 	);
 };
