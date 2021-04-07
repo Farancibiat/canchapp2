b07608572ed210c55635be000bc72ad095cb8fc9
Farancibiat <arancibiat.felipe@gmail.com>
2021-04-07 02:02:34 +0000

    emailjs implementado a medias, falta relacionar formulario con fetch de email js

--- src/front/js/component/reservationfields.js
+++ src/front/js/component/reservationfields.js
@@ -1,6 +1,7 @@
 // import Tools
 import React, { useState, useContext } from "react";
 import Datepicker from "react-datepicker";
+import emailjs from "emailjs-com";
 
 // import css
 import "../styles/agendar.css";
@@ -12,6 +13,44 @@ import Cancha from "../../img/img_reserva.jpg";
 export const ReservationFields = () => {
 	const [selectDate, setSelectDate] = useState(null);
 
+	function sendEmail(e) {
+		e.preventDefault();
+
+		emailjs
+			.send(
+				"pichangapp_s26kmmb",
+				"rent_u94nq7m",
+				{
+					complex_name: "Estadio Monumental",
+					to_name: "Hector",
+					id_reserva: "007",
+					fecha: "27/05/2021",
+					hora: "21",
+					precio: "10.000",
+					tshirts: "Si",
+					tshirts_price: "1.000",
+					ball: "Si",
+					ball_price: "1.000",
+					referee: "Si",
+					referee_price: "15.000",
+					precio_final: "27.000",
+					complex_mail: "arancibiat.felipe@gmail.com",
+					complex_phone: "+5695797420",
+					user_mail: "hectorsalas7@gmail.com",
+					reply_to: "ces.acevedo.23@gmail.com"
+				},
+				"user_F3htLlSg7bVzumwkoOdNw"
+			)
+			.then(
+				result => {
+					console.log(result.text);
+				},
+				error => {
+					console.log(error.text);
+				}
+			);
+	}
+
 	return (
 		<div className="agendamiento">
 			<div className="contenedor_agenda">
@@ -20,8 +59,8 @@ export const ReservationFields = () => {
 					En este apartado podras elegir la fecha que necesites para jugar tu{" "}
 					<span className="text_pichanga">Pichanga:</span>
 				</p>
-				<div className="fila">
-					<form className="formulario_seleccion">
+				<form className="formulario_seleccion" onSubmit={sendEmail}>
+					<div className="fila">
 						<div className="contenedor_verde">
 							<h4>Selecciona la fecha:</h4>
 							<Datepicker
@@ -49,10 +88,12 @@ export const ReservationFields = () => {
 								<h5> Balon</h5>
 							</div>
 						</div>
-					</form>
-				</div>
-				<button className="btn btn-success">Procesar</button>
-				<img src={Cancha} alt="Small Court" />
+					</div>
+					<button type="submit" className="btn btn-success">
+						Procesar
+					</button>
+					<img src={Cancha} alt="Small Court" />
+				</form>
 			</div>
 		</div>
 	);
