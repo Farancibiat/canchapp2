88095b1f82a9e955a2129fc54541e4cf65508888
Cesar-Antonio23 <ces.acevedo.23@gmail.com>
2021-04-09 21:05:35 +0000

    cambios

--- src/front/js/component/recoverField.js
+++ src/front/js/component/recoverField.js
@@ -3,28 +3,17 @@ import { Context } from "../store/appContext";
 import "../styles/login.css";
 import { Link } from "react-router-dom";
 
-export const Login = () => {
+export const RecoverField = () => {
 	const { store, actions } = useContext(Context);
 	const [email, setEmail] = useState("");
-	const [password, setPassword] = useState("");
-	const [remember, setRemember] = useState(false);
 
 	const handlerSubmit = e => {
 		e.preventDefault();
 
-		actions.setLogin(
-			{
-				email: email,
-				password: password
-			},
-			remember
-		);
+		actions.recover({
+			email: email
+		});
 	};
-	function handleChange(event) {
-		const input = event.target;
-		const value = input.type === "checkbox" ? input.checked : input.value;
-		setRemember(value);
-	}
 
 	return (
 		<div className="fondo-login justify-content-center">
@@ -32,11 +21,11 @@ export const Login = () => {
 				<div className="d-flex justify-content-center h-100">
 					<div className="card-login rounded-lg">
 						<div className="card-header">
-							<h3>Sign In</h3>
+							<h3>Recuperar</h3>
 						</div>
 						<div className="card-body">
 							<form onSubmit={e => handlerSubmit(e)}>
-								<div className="input-group form-group">
+								<div className="input-group form-group py-5">
 									<div className="input-group-prepend">
 										<span className="input-group-text">
 											<i className="fas fa-envelope" />
@@ -50,7 +39,7 @@ export const Login = () => {
 										onChange={e => setEmail(e.target.value)}
 									/>
 								</div>
-								<div className="input-group form-group">
+								{/* <div className="input-group form-group">
 									<div className="input-group-prepend">
 										<span className="input-group-text">
 											<i className="fas fa-key" />
@@ -63,31 +52,23 @@ export const Login = () => {
 										placeholder="Contraseña"
 										onChange={e => setPassword(e.target.value)}
 									/>
-								</div>
-								<div className="row align-items-center remember">
-									<input
-										name="remember"
-										checked={remember}
-										onChange={e => handleChange(e)}
-										type="checkbox"
-										id="RememberMe"
-									/>
-								</div>
+								</div> */}
 								<div className=" form-group">
-									<input type="submit" value="Login" className="btn float-right login_btn" />
+									<input type="submit" value="Enviar" className="btn float-right login_btn" />
 								</div>
 							</form>
 						</div>
-						<div className="card-footer">
+						{/* <div className="card-footer">
 							<div className="d-flex justify-content-center links" />
 							<div className="d-flex justify-content-center">
-								<Link to={"/recover/"}>Olvidaste tu contraseña?</Link>
+								<Link to="#">Olvidaste tu contraseña?</Link>
 							</div>
-						</div>
+						</div> */}
 					</div>
 				</div>
 			</div>
 		</div>
 	);
 };
-// onClick={setRemember(!remember)}
+
+export default RecoverField;
